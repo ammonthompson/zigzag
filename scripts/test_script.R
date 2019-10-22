d45506f07dd7052b349fecd01c8b7caebde47a8d
DESKTOP-82209V4\ammon_work <ammonthompson@gmail.com>
2019-10-22 09:31:27 -0700

    no message

--- scripts/test_script.R
+++ scripts/test_script.R
@@ -1,22 +1,23 @@
 # read some data
 
 
-# dat <- read.table("../../../bitbucket_repos/nmix_data_files/gtex_data/allgenes_liver_gtex.tpm", header=TRUE, row.names=1)
-dat <- read.table("../simulate_data/simulated_data/sim3_twoComps_2mu3_gene_length.tsv", header=TRUE, row.names=1)
+dat <- read.table("../../nmix_data_files/gtex_data/subfile1_25libs_Lung_RNA.tpm", header=TRUE, row.names=1)
+#dat <- read.table("../simulate_data/simulated_data/sim3_twoComps_2mu3_nactive2500.tsv", header=TRUE, row.names=1)
 
-gene_length_df = read.table("../simulate_data/simulated_data/sim3_twoComps_2mu3_gene_length.tsv", row.names = 1, header = TRUE)
+gene_length_df = read.table("../../nmix_data_files/gtex_data/protCoding_GTEx_hg19_meanLength.txt",
+                            row.names = 1, header = FALSE)
 
 
 sq=rbind(c(1,2),c(3,4)); layout(sq)
 
-mm <- zigzag$new(data = dat, gene_length = gene_length_df, candidate_gene_list = "random",
-                    output_directory = "../testing", num_active_components =2,
-                    active_means_dif_prior_shape = 20000, active_means_dif_prior_rate = 10000,
-                    threshold_i = -1, threshold_a = c(0, 1), active_gene_set = NULL, shared_active_variances = T, beta = 1)
+mm <- zigzag$new(data = as.data.frame(dat[1:2000,1]), gene_length = gene_length_df[1:2000,1], candidate_gene_list = "random",
+                    output_directory = "../testing", num_active_components =3,
+                    active_means_dif_prior_shape = 1, active_means_dif_prior_rate = 1,
+                    threshold_i = -1, threshold_a = c(1, 4, 6), active_gene_set = NULL, shared_active_variances = T, beta = 1)
 
 
 mm$burnin(sample_frequency = 20, burnin_target_acceptance_rate=0.44, progress_plot = T,
-          write_to_files = T, ngen=5000, append = F)
+          write_to_files = T, ngen=25000, append = F)
 
 
 mm$mcmc(sample_frequency = 50, progress_plot = F, write_to_files = T, ngen=50000, append = F,
